2fa59b58cd566e0ff7f38e53abfee496b9667f9b
GuglielmoBini <guglielmobini@gmail.com>
2023-03-10 16:26:27 +0100

    migrated and seeded types table

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,16 +2,25 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $labels = ['Front-End', 'Back-End', 'API'];
+
+        foreach ($labels as $label) {
+            $type = new Type();
+            $type->label = $label;
+            $type->color = $faker->hexColor();
+            $type->save();
+        }
     }
 }
