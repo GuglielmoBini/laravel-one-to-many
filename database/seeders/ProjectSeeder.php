9dedd6a48d12358ad6c93013269f17e0305a9c42
GuglielmoBini <guglielmobini@gmail.com>
2023-03-11 18:00:06 +0100

    randomized types

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use Illuminate\Support\Arr;
 
 class ProjectSeeder extends Seeder
 {
@@ -14,8 +16,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        for ($i = 0; $i < 15; $i++) {
+        $type_ids = Type::select('id')->pluck('id')->toArray();
+        $type_ids[] = null;
+
+        for ($i = 0; $i < 30; $i++) {
             $project = new Project();
+            $project->type_id = Arr::random($type_ids);
             $project->name = $faker->words(3, true);
             $project->project_url = $faker->url();
             // $project->image_url = $faker->imageUrl(250, 250);
